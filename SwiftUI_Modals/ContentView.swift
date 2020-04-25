32d8a9a08ea106a2c10a648ff9f195187fb54784
rbt200 <myesoap@mail.ru>
2020-04-25 05:48:22 -0700

    Send data to modal view

--- SwiftUI_Modals/ContentView.swift
+++ SwiftUI_Modals/ContentView.swift
@@ -11,13 +11,39 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showModal: Bool = false
+    @State private var selectedFlag: String = ""
+    
+    let flags = ["🇦🇽", "🇦🇱", "🇩🇿", "🇦🇸", "🇦🇩", "🇦🇴", "🇦🇮"]
     
     var body: some View {
-        Button("Show modal") {
-            self.showModal.toggle()
-        }.sheet(isPresented: $showModal) {
-            Text("I am a modal")
+        
+        List {
+            
+            ForEach(0..<self.flags.count) { index in
+                HStack {
+                    Text(self.flags[index])
+                        .font(.custom("Arial", size: 100))
+                    Text("Flag \(index)")
+                }.onTapGesture {
+                    self.showModal.toggle()
+                    self.selectedFlag = self.flags[index]
+                }
+                
+            }
+            
+        }.sheet(isPresented: self.$showModal) {
+            HStack {
+                Text(self.selectedFlag)
+                    .font(.custom("Arial", size:     200))
+            }
         }
+            
+        
+//        Button("Show modal") {
+//            self.showModal.toggle()
+//        }.sheet(isPresented: $showModal) {
+//            Text("I am a modal")
+//        }
     }
 }
 
